16792ac60bdeada7520550ead02825b1bb07aea1
Emanuele <eminik2006@gmail.com>
2023-01-31 16:29:29 +0100

    Created scaling functions, image canvas

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import hydra
 import pandas as pd
+import numpy as np
 import re
 import os
 import csv
 import preprocessing 
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.preprocessing import minmax_scale
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="config")
@@ -20,25 +24,69 @@ def main(config):
         # Debug compute only the first folder of the folder list
         if count_var:    
             # Get all the .csv files (COMPLETE PATH) inside the folder 
-            file_list = [f.path for f in os.scandir(folder) if f.is_file() and f.path.endswith(config.file_extension)]
+            task_file_list = [f.path for f in os.scandir(folder) if f.is_file() and f.path.endswith(config.file_extension)]
             
             # Sort the file based on filename
-            file_list = sorted(file_list, key = lambda x: len(x))
+            task_file_list = sorted(task_file_list, key = lambda x: len(x))
             
-            for file in file_list:
+            for task in task_file_list:
                 
                 # Debug compute only the first file in the folder
                 if count_var:     
                     
-                    print(f"Filename: {file} \n")
+                    print(f"Filename: {task} \n")
                     
-                    file_dataframe = preprocessing.dataframe_from_csv(file)
+                    task_dataframe = preprocessing.dataframe_from_csv(task)
                     
-                    print(f"Dataframe: \n {file_dataframe} \n")
+                    # print(f"Dataframe: \n {task_dataframe} \n")
                     
-                    #file_dataframe = preprocessing.filter_dataframe_rows_by_value(file_dataframe, 'Phase', 'MoveStroke')
-                                        
-                    #print(f"Dataframe: \n {file_dataframe} \n")
+                    task_dataframe = preprocessing.points_type_filtering(task_dataframe,"onpaper")
+                  
+                    print(f"Dataframe: \n {task_dataframe} \n")
+                    
+                    # Extract column from dataframe and convert to numpy array    
+                    x = preprocessing.dataframe_column_to_array(task_dataframe,"PointX")
+                    y = preprocessing.dataframe_column_to_array(task_dataframe,"PointY")
+                    
+                    # Remove the Wacom One offset from points
+                    x,y = preprocessing.remove_offset_points(x,y)
+                    
+                    # Compute Speed and Acceleration from Points (x,y)
+                    vel,acc = preprocessing.compute_speed_and_acceleration(x,y)
+                    
+                    print(f"Num of points ({len(vel)}) \n Velocity: {vel} \n Acceleration: {acc} \n")
+                
+                    #Canvas dimensions
+                    height = 1080 
+                    width = 1920
+                    thickness = 3
+                    color = [255,0,0]
+                
+                    # Array Scaling with MinMaxScaler
+                    x = np.int_(minmax_scale(x, feature_range=(0,width)))
+                    y = np.int_(minmax_scale(y, feature_range=(0,height)))
+                    
+                    # Merge 2 array 
+                    points = np.column_stack((x, y))
+                    
+                    print(points)
+                    
+                    # Create Image Matrix
+                    image = np.zeros((height, width, 3), np.uint8)
+                    
+                    # Fill the Image with white color
+                    image.fill(255)
+                    
+                    # Loop through all the points for drawing on the canvas
+                    for i in range(1, len(points)):
+                        start = tuple(points[i - 1])
+                        end = tuple(points[i])
+                        cv2.line(image, start, end, color, thickness)
+                    
+                    # Show the image in a new Window
+                    cv2.imshow("Image", image)
+                    cv2.waitKey(0)
+
   
                     count_var = False
                 else:
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import minmax_scale
 
 
 def dataframe_from_csv(file_csv: str) -> pd.DataFrame:
@@ -41,23 +43,99 @@ def dataframe_from_csv(file_csv: str) -> pd.DataFrame:
     
     # Re-order dataframe columns 
     df = df[header + ['Timestamp']]
+    
+    df = df.astype({'PointX': 'int32', 'PointY': 'int32','Pressure': 'int32', 'Rotation': 'int32', 'Azimuth': 'int32', 'Altitude': 'int32', 'TiltX': 'int32', 'TiltY': 'int32'})
   
     return df
 
-def filter_dataframe_rows_by_value(df:pd.DataFrame, column:str, value_to_filter: str) -> pd.DataFrame:
+
+def points_type_filtering(df:pd.DataFrame, points_type:str = "onpaper") -> pd.DataFrame:
     """
-    This function takes a dataframe, a column name, and a value to filter by, and returns a dataframe
-    with only the rows that match the value
+    It takes a dataframe and a string as input, and returns a dataframe filtered by pressure
     
     :param df: the dataframe you want to filter
     :type df: pd.DataFrame
-    :param column: the column name in the dataframe that you want to filter by
-    :type column: str
-    :param value_to_filter: The value you want to filter by
-    :type value_to_filter: str
+    :param points_type: str = "onpaper", defaults to onpaper
+    :type points_type: str (optional)
     :return: A dataframe
     """
     
-    df = df.loc[df[column] == value_to_filter].reset_index(drop=True)
+    if points_type == "onpaper":
+        
+        df = df.loc[df["Pressure"] != 0].reset_index(drop=True)
+        
+    elif points_type == "onair":
+        
+        df = df.loc[df["Pressure"] == 0].reset_index(drop=True)
+        
+    else:
+        pass
     
     return df
+
+
+def dataframe_column_to_array(df:pd.DataFrame, column_name:str) -> np.array:
+    """
+    > This function takes a dataframe and a column name and returns a numpy array of the values in that
+    column
+    
+    :param df: The dataframe that contains the data
+    :type df: pd.DataFrame
+    :param column_name: The name of the column you want to convert to an array
+    :type column_name: str
+    :return: A numpy array
+    """
+    return df[column_name].to_numpy()
+
+
+def remove_offset_points(x_array:np.array, y_array:np.array):
+    """
+    It takes in two arrays, subtracts 0 from each element in the array, and returns the two arrays
+    
+    :param x_array: the x-coordinates of the points
+    :type x_array: np.array
+    :param y_array: the y-coordinates of the points
+    :type y_array: np.array
+    :return: The x and y arrays with the offset points removed.
+    """
+    
+    x_array = x_array - 0
+    
+    y_array = y_array - 0
+    
+    return x_array, y_array
+    
+
+def scaling_array(array:np.array, max_range:int) -> np.array:
+    """
+    It takes an array, subtracts the minimum value from each element, divides each element by the
+    maximum value minus the minimum value, and then multiplies each element by 255
+    
+    :param array: the array to be scaled
+    :type array: np.array
+    :return: The array is being returned.
+    """
+    
+    array = ((array - array.min()) * (1/(array.max() - array.min()) * max_range)).astype('uint8')
+    
+    return array
+
+
+def compute_speed_and_acceleration(x:np.array, y:np.array):
+    """
+    It computes the speed and acceleration of the traits given its x and y coordinates
+    
+    :param x: the x-coordinates of the points
+    :type x: np.array
+    :param y: the y-coordinates of the trajectory
+    :type y: np.array
+    :return: the speed and acceleration of the object.
+    """
+   
+    dx = np.diff(x)
+    dy = np.diff(y)
+    dt = np.diff(np.arange(len(x))) 
+    v = np.sqrt(dx**2 + dy**2) / dt
+    dv = np.diff(v)
+    a = dv / dt[:-1]
+    return v,a
