015d81910ade530cf3c099d6c29a4815079145c8
Emanuele <eminik2006@gmail.com>
2023-04-24 17:15:51 +0200

    [FIX] Updated README.md and config.yaml

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
 # pyHEXtraction
 
-## Prerequisites after first pull
+## Prerequisites after Cloning the repository:
 
-1. Create folder for data:
-    ```
-    create a folder called "data"
-    ```
-2. Create Python Virtual Environment:
+1. Create Python Virtual Environment:
     ```
     python -m venv env
     ```
-3. Activate Virtual Environment:<br>
+2. Activate Virtual Environment:<br>
     **Linux**
     ```
     source env/bin/activate
@@ -19,15 +15,15 @@
     ```
     env\Scripts\activate
     ```
-4. Load Required libraries
+3. Load Required libraries
     ```
     pip install -r requirements.txt
     ```
-5. Give Script permissions in Linux:<br>
+4. Give Script permissions in Linux:<br>
     ```
     chmod +x main.py
     ```
-6. Deactivate Virtual Environment:<br>
+5. Deactivate Virtual Environment:<br>
     **Linux**
     ```
     source env/bin/deactivate
@@ -65,6 +61,53 @@ python -m pip freeze > requirements.txt
     ```
     env\Scripts\deactivate
     ```
+
+## Run the Project on PyCharm
+
+After creating the virtual environment, you can run the project on PyCharm.
+
+**Steps:**
+
+1. Open the project on PyCharm
+2. Open the Terminal
+3. Go to Settings -> Project -> Project Interpreter
+4. Click on the Gear Icon and select Add Local
+5. Select the virtual environment folder
+6. Open main.py and run it
+
+## General Development Workflow
+
+1. Create a new local branch from the master branch
+   ```
+    git checkout -b <branch_name>
+    ```
+2. Make changes to the code on a separate python file
+3. Once the feature is complete, start update the modified files on the local branch
+4. Add the changes to the staging area
+    ```
+    git add <file_name>
+    ```
+5. Commit the changes
+    ```
+    git commit -m "<commit_message>"
+    ```
+6. Push the changes to the remote repository
+    ```
+    git push origin <branch_name>
+    ```
+7. Create a pull request on GitHub
+8. Once the pull request is approved, merge the branch to the master branch
+9. Delete the local branch
+    ```
+    git branch -d <branch_name>
+    ```
+10. Delete the remote branch
+    ```
+      git push origin --delete <branch_name>
+    ```
+
+## License
+This project is licensed under the @Università degli Studi di Cassino e del Lazio Meridionale
 
 ## Notes
 On Windows:
--- src/main.py
+++ src/main.py
@@ -5,76 +5,74 @@ sys.dont_write_bytecode = True
 import hydra
 import os
 import pandas as pd
-import preprocessing 
+import preprocessing
 import feature_extraction_module
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="config")
 def main(config):
-    
-    # Check data directory  
-    try: 
+    # Check data directory
+    try:
         # If directory has not yet been created
         os.makedirs(config.settings.data_source)
-        
+
         sys.exit("Empty directory data")
-    except:
+    except OSError as excep:
         if not os.listdir(config.settings.data_source):
-            sys.exit("Empty directory data")   
-        else:    
+            sys.exit("Empty directory data")
+        else:
             # Get all the folders (complete PATH) inside data (to get only folder name replace f.path with f.name)
             folders_in_data = [f.path for f in os.scandir(config.settings.data_source) if f.is_dir()]
-    
+
     # Check background images directory  
-    try: 
+    try:
         # If directory has not yet been created
         os.makedirs(config.settings.background_images_folder)
-        
+
         sys.exit("Empty background images folder")
-    except:
+    except OSError as excep:
         if not os.listdir(config.settings.background_images_folder):
-            sys.exit("Empty background images folder")   
-        else:    
+            sys.exit("Empty background images folder")
+        else:
             # Get the background_images
             background_images_list = preprocessing.get_images_in_folder(config.settings.background_images_folder)
-    
+
     # Iterate over the folders in data
     for folder in folders_in_data:
-        
+
         # list of the images file in the subject folder
         # images_folder = [f.path for f in os.scandir(folder) if f.is_dir()]
-        
+
         # Process the images in Images folder if necessary
         # preprocessing.process_images_files(images_folder,config.settings.images_extension)
-        
+
         # Get all the .csv files (COMPLETE PATH) inside the folder 
-        task_file_list = [f.path for f in os.scandir(folder) if f.is_file() and f.path.endswith(config.settings.file_extension)]
-                    
+        task_file_list = [f.path for f in os.scandir(folder) if
+                          f.is_file() and f.path.endswith(config.settings.file_extension)]
+
         # Sort the file based on filename
-        task_file_list = sorted(task_file_list, key = lambda x: len(x))
-        
+        task_file_list = sorted(task_file_list, key=lambda x: len(x))
+
         # Iterate over the task file in the data folder and the background image
         for task_number, (task, bck_image) in enumerate(zip(task_file_list, background_images_list)):
-            
+
             # Debug compute only the first "n" file in the folder
             if task_number == 1:
-                 
-                                
                 # Create the dataframe from the csv data
                 task_dataframe = preprocessing.load_data_from_csv(task)
-                                    
+
                 # Filter the dataframe by the type of points (onair / onpaper)
                 # task_dataframe = preprocessing.points_type_filtering(task_dataframe,"onpaper")
-                         
+
                 # Correct the coordinates system from Digitizer origin -> Image Standard origin
                 task_dataframe = preprocessing.coordinates_manipulation(task_dataframe)
-                
+
                 # Show image of the current task created from the csv
                 # preprocessing.create_image_from_data(task_dataframe["PointX"].to_numpy(), 
                 #                                      task_dataframe["PointY"].to_numpy(), 
                 #                                      task_dataframe["Pressure"].to_numpy(),
                 #                                      bck_image, task, config)
-                
+
                 # Show image-video of the current task created from the csv
                 # preprocessing.create_gif_from_data(task_dataframe["PointX"].to_numpy(), 
                 #                                      task_dataframe["PointY"].to_numpy(), 
@@ -83,14 +81,14 @@ def main(config):
 
                 # Print the csv data of the current task
                 print(f"[+] Task Preprocessed: \n {task_dataframe} \n")
-                
+
                 print(f"[+] Starting Feature Extraction \n")
-                
+
             # else:
             #     break  
-               
+
     return
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     main()
-    
